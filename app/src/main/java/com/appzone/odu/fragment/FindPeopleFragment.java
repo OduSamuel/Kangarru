dbd3ffae30cd6bccb0aba426e8eb1a6abae4aced
unknown <Odu@Odu.appzonegroup.local>
2015-02-25 08:45:29 +0100

    Initial commit

---
+++ app/src/main/java/com/appzone/odu/fragment/FindPeopleFragment.java
@@ -0,0 +1,21 @@
+package com.appzone.odu.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.appzone.odu.snape.R;
+public class FindPeopleFragment extends Fragment{
+    public FindPeopleFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_find_people, container, false);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/appzone/odu/fragment/WhatsHotFragment.java
@@ -0,0 +1,23 @@
+package com.appzone.odu.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.appzone.odu.snape.R;
+
+public class WhatsHotFragment extends Fragment {
+
+    public WhatsHotFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_whats_hot, container, false);
+
+        return rootView;
+    }
+}
